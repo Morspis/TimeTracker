b30b72bd4bc0213a0182ef9d8d5df5ae8e3a997e
Fixxzle <walsere@mail.gvsu>
2021-02-17 20:18:17 -0500

    updated front end

--- summa-time/src/app/app-routing.module.ts
+++ summa-time/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+
 
 const routes: Routes = [
-  {path: 'home', component: AppComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'homefd', component: AppComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'projects', component: ProjectsComponent}
+  
+  
 ];
 
 @NgModule({
--- summa-time/src/app/app.module.ts
+++ summa-time/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
