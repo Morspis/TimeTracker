32dade670ea10e749eca619f8ac0f33bf6ab3d99
Morspis <dyst@mail.gvsu.edu>
2021-02-22 14:10:15 -0500

    got angular communicating with node server

--- summa-time/src/app/app.module.ts
+++ summa-time/src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { DisplayBoardComponent } from './display-board/display-board.component';
+import { UsersComponent } from './users/users.component';
+import { environment } from '../environments/environment';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    DisplayBoardComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
