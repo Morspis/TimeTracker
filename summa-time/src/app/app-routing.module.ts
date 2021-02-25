e8db45038083d827cdb1c3535e59987a8c4de179
Morspis <dyst@mail.gvsu.edu>
2021-02-24 19:29:25 -0500

    deleted unnecessary components

--- summa-time/src/app/app-routing.module.ts
+++ summa-time/src/app/app-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'homefd', component: AppComponent},
+  {path: 'projects', component: ProjectsComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'projects', component: ProjectsComponent}
-  
-  
 ];
 
 @NgModule({
